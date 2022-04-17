0f2f498030b183759b613094ebc2b830ec6f2169
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2022-04-17 21:02:14 +0200

    variants

--- flake.nix
+++ flake.nix
@@ -7,32 +7,36 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      config = pkgs.writeTextFile {
-        name = "vale.ini";
-        text = ''
-          StylesPath = ${./styles}
-          MinAlertLevel = suggestion
+      styles = builtins.attrNames (builtins.readDir ./styles);
+      all_styles =
+        "alex, Google, Vale, Microsoft, proselint, Readability, Vocab, write-good";
+      config = style:
+        pkgs.writeTextFile {
+          name = "vale.ini";
+          text = ''
+            StylesPath = ${./styles}
+            MinAlertLevel = suggestion
 
-          Vocab = Base
+            Vocab = Base
 
-          [*]
-          BasedOnStyles = alex, Google, Vale, Microsoft, proselint, Readability, Vocab, write-good
-                 '';
-        executable = false;
-      };
-      valenix = pkgs.writeScriptBin "valenix" ''
-        #!${pkgs.stdenv.shell}
-        ${pkgs.vale}/bin/vale --config=${config} $@
-      '';
+            [*]
+            BasedOnStyles = ${style}
+                   '';
+          executable = false;
+        };
+      valenix_style = style: style_name:
+        let config_style = config style;
+        in pkgs.writeScriptBin "valenix_${style_name}" ''
+          #!${pkgs.stdenv.shell}
+          ${pkgs.vale}/bin/vale --config=${config_style} $@
+        '';
+      valenix = valenix_style all_styles "all";
+      valenix_versions = builtins.listToAttrs (builtins.map (style: {
+        name = "valenix_${style}";
+        value = valenix_style style style;
+      }) styles);
     in {
-      packages.${system} = {
-        inherit valenix config;
-        default = valenix;
-      };
+      packages.${system} = valenix_versions // { inherit valenix; };
       defaultPackage.${system} = valenix;
-      defaultApp.${system} = {
-        type = "app";
-        program = "${valenix}/bin/valenix";
-      };
     };
 }
